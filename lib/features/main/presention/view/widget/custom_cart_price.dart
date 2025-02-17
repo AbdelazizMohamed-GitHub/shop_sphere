07897856fe511154ae5b37fa1c27a16d2b8780bd
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-02-17 23:38:54 +0200

    Refactor CartScreen

---
+++ lib/features/main/presention/view/widget/custom_cart_price.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:shop_sphere/core/constant/app_styles.dart';
+
+class CustomCartPrice extends StatelessWidget {
+ const  CustomCartPrice({
+    super.key,
+    required this.title,
+    required this.price,
+    this.isTotalcoast = false,
+  });
+  final String title;
+  final double price;
+ final  bool  isTotalcoast;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: AppStyles.text22SemiBoldBlack),
+        Text('\$${price.toStringAsFixed(2)}',
+            style:isTotalcoast?AppStyles.text18RegularBlack.copyWith(color: Colors.green): AppStyles.text18RegularBlack),
+      ],
+    );
+  }
+}
