0907483178c444b019d83cc753a9a834a767d210
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-04-03 23:54:44 +0200

    Add stream to Cart screen To Real  Time Update

--- lib/features/profile/presention/view/screen/cart_screen.dart
+++ lib/features/profile/presention/view/screen/cart_screen.dart
@@ -1,15 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop_sphere/core/service/setup_locator.dart';
-import 'package:shop_sphere/core/test_data/test_list.dart';
 import 'package:shop_sphere/core/utils/app_theme.dart';
 import 'package:shop_sphere/core/widget/custom_back_button.dart';
 import 'package:shop_sphere/core/widget/custom_button.dart';
 import 'package:shop_sphere/features/explor/data/model/cart_model.dart';
-import 'package:shop_sphere/features/explor/data/repo_impl/cart_repo_impl.dart';
 import 'package:shop_sphere/features/explor/domain/entity/cart_entity.dart';
-import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_cubit.dart';
-import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_state.dart';
 import 'package:shop_sphere/features/profile/presention/view/screen/checkout_screen.dart';
 import 'package:shop_sphere/features/profile/presention/view/widget/custom_cart_item_list.dart';
 import 'package:shop_sphere/features/profile/presention/view/widget/custom_cart_price.dart';
@@ -19,85 +15,83 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          CartCubit(cartRepo: getIt<CartRepoImpl>())..getAllProductsInCart(),
-      child: Scaffold(
-        appBar: AppBar(
-          leadingWidth: 100,
-          title: const Text('My Cart'),
-          leading: AppTheme.isLightTheme(context)
-              ? const CustomBackButton()
-              : IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.arrow_back_ios_new)),
-        ),
-        body: BlocBuilder<CartCubit, CartState>(
-          builder: (context, state) {
-            if (state is CartLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (state is CartFailure) {
-              return Center(
-                child: Text(state.errMessage),
-              );
-            }
-            if (state is GetCartSuccess) {
-              if (state.cartItems.isEmpty) {
-                return const Center(
-                  child: Text('Your cart is empty'),
-                );
-              }
-            }
-            if (state is GetCartSuccess) {
-             
-              return Column(
-                children: [
-                  Expanded(
-                      child: CustomCartItemList(
-                    cartItems: state.cartItems
-                        .cast<CartEntity>(), // Cast to List<CartItemModel>
-                  )),
-                  Container(
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    )),
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(children: [
-                      CustomCartPrice(
-                          title: 'Total:', price: state.total.toDouble()),
-                      const CustomCartPrice(title: 'Shipping:', price: 50),
-                      const Divider(),
-                      CustomCartPrice(
-                          title: 'Total Cost:',
-                          price: state.total + 50,
-                          isTotalcoast: true),
-                      const SizedBox(height: 20.0),
-                      CustomButton(
-                          onPressed: () {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) {
-                              return const CheckoutScreen();
-                            }));
-                          },
-                          text: "Checkout")
-                    ]),
-                  ),
-                ],
-              );
-            }
-            return const Center(
-              child: Text('Something went wrong'),
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 100,
+        title: const Text('My Cart'),
+        leading: AppTheme.isLightTheme(context)
+            ? const CustomBackButton()
+            : IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios_new)),
       ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .collection("cart")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Error'));
+            } else if (snapshot.hasData) {
+              List<CartEntity> cartItems = snapshot.data!.docs.map((doc) {
+                return CartItemModel.fromMap(doc.data());
+              }).toList();
+              if (cartItems.isEmpty) {
+                return const Center(child: Text('Your cart is empty'));
+              }else {
+                // Calculate the total cost
+                double total = 0.0;
+                for (var item in cartItems) {
+                  total += item.productPrice * item.productQuantity;
+                }
+                 return Column(
+              children: [
+                Expanded(
+                    child: CustomCartItemList(
+                  cartItems: cartItems
+                      .cast<CartEntity>(), // Cast to List<CartItemModel>
+                )),
+                Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )),
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(children: [
+                    CustomCartPrice(
+                        title: 'Total:', price: total),
+                    const CustomCartPrice(title: 'Shipping:', price: 50),
+                    const Divider(),
+                    CustomCartPrice(
+                        title: 'Total Cost:',
+                        price: total + 50,
+                        isTotalcoast: true),
+                    const SizedBox(height: 20.0),
+                    CustomButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const CheckoutScreen();
+                          }));
+                        },
+                        text: "Checkout")
+                  ]),
+                ),
+              ],
+            );
+              }
+             
+            }
+            return const Center(child: Text('No Data'));
+          }),
+      
     );
   }
 }
--- lib/features/profile/presention/view/widget/custom_cart_item.dart
+++ lib/features/profile/presention/view/widget/custom_cart_item.dart
@@ -2,11 +2,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_sphere/core/service/setup_locator.dart';
 import 'package:shop_sphere/core/utils/app_color.dart';
 import 'package:shop_sphere/core/utils/app_images.dart';
 import 'package:shop_sphere/core/utils/app_styles.dart';
 import 'package:shop_sphere/core/utils/app_theme.dart';
 import 'package:shop_sphere/core/widget/custom_circle_button.dart';
+import 'package:shop_sphere/features/explor/data/repo_impl/cart_repo_impl.dart';
 import 'package:shop_sphere/features/explor/domain/entity/cart_entity.dart';
 import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_cubit.dart';
 import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_state.dart';
@@ -19,87 +21,91 @@ class CustomCartItem extends StatelessWidget {
   final CartEntity item;
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CartCubit, CartState>(
-      builder: (context, state) {
-        return Card(
-          color: AppTheme.isLightTheme(context)
-              ? Colors.white
-              : AppColors.secondaryDarkColor,
-          child: ListTile(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-            leading: Container(
-              width: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppTheme.isLightTheme(context)
-                    ? Colors.white
-                    : AppColors.backgroundDarkColor,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: CachedNetworkImage(
-                  fit: BoxFit.cover,
-                  imageUrl: item.productImage,
-                  placeholder: (context, url) => const Center(
-                    child: CircularProgressIndicator(),
+    return BlocProvider(
+      create: (context) => CartCubit(cartRepo: getIt<CartRepoImpl>()),
+      child: BlocBuilder<CartCubit, CartState>(
+        builder: (context, state) {
+          return Card(
+            color: AppTheme.isLightTheme(context)
+                ? Colors.white
+                : AppColors.secondaryDarkColor,
+            child: ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+              leading: Container(
+                width: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppTheme.isLightTheme(context)
+                      ? Colors.white
+                      : AppColors.backgroundDarkColor,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: CachedNetworkImage(
+                    fit: BoxFit.cover,
+                    imageUrl: item.productImage,
+                    placeholder: (context, url) => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
               ),
-            ),
-            title: Text(
-              item.productName,
-              style: AppStyles.text18Regular.copyWith(
-                  color: AppTheme.isLightTheme(context)
-                      ? Colors.black
-                      : Colors.white),
-            ),
-            subtitle: Text(
-                '\$${item.productPrice.toStringAsFixed(2)} x ${item.productQuantity}',
-                style: AppStyles.text14Regular.copyWith(
+              title: Text(
+                item.productName,
+                style: AppStyles.text18Regular.copyWith(
                     color: AppTheme.isLightTheme(context)
                         ? Colors.black
-                        : Colors.white)),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CustomCircleButton(
-                    icon: Icon(Icons.remove),
-                    funcation: () async {
-                      await context.read<CartCubit>().updateCartQuantity(
-                          productId: item.productId, isIncrement: false);
-                      context.read<CartCubit>().getAllProductsInCart();
-                    }),
-                const SizedBox(
-                  width: 20,
-                ),
-                CustomCircleButton(
-                    icon: const Icon(Icons.add),
-                    funcation: () async {
-                      await context.read<CartCubit>().updateCartQuantity(
-                          productId: item.productId, isIncrement: true);
-                      context.read<CartCubit>().getAllProductsInCart();
-                    }),
-                const SizedBox(
-                  width: 50,
-                ),
-                IconButton(
-                    onPressed: () async {
-                      await context
-                          .read<CartCubit>()
-                          .removeFromCart(productId: item.productId);
+                        : Colors.white),
+              ),
+              subtitle: Text(
+                  '\$${item.productPrice.toStringAsFixed(2)} x ${item.productQuantity}',
+                  style: AppStyles.text14Regular.copyWith(
+                      color: AppTheme.isLightTheme(context)
+                          ? Colors.black
+                          : Colors.white)),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CustomCircleButton(
+                      icon: Icon(Icons.remove),
+                      funcation: () async {
+                        await context.read<CartCubit>().updateCartQuantity(
+                            productId: item.productId, isIncrement: false);
+                        context.read<CartCubit>().getAllProductsInCart();
+                      }),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CustomCircleButton(
+                      icon: const Icon(Icons.add),
+                      funcation: () async {
+                        await context.read<CartCubit>().updateCartQuantity(
+                            productId: item.productId, isIncrement: true);
+                        context.read<CartCubit>().getAllProductsInCart();
+                      }),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  IconButton(
+                      onPressed: () async {
+                        await context
+                            .read<CartCubit>()
+                            .removeFromCart(productId: item.productId);
 
-                      context.read<CartCubit>().getAllProductsInCart();
-                    },
-                    icon: const Icon(
-                      Icons.delete,
-                      color: AppColors.primaryColor,
-                    ))
-              ],
+                        context.read<CartCubit>().getAllProductsInCart();
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: AppColors.primaryColor,
+                      ))
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
