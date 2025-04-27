072f44b8d99064aff72d657942ec142bc73734aa
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-04-27 10:44:46 +0300

    Refactor NotificationModel to use Hive for local storage and update dependencies in pubspec.yaml

--- lib/features/main/data/notification_model.dart
+++ lib/features/main/data/notification_model.dart
@@ -1,7 +1,15 @@
-class NotificationModel {
+import 'package:hive/hive.dart';
+part 'notification_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotificationModel extends HiveObject {
+ 
+  @HiveField(0)
 
   final String title;
+@HiveField(1)
   final String description;
+@HiveField(2)
   final String date;
 
   NotificationModel({required this.title, required this.description, required this.date});
