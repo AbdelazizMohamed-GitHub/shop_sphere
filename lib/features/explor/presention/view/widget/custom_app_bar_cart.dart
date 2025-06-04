cd2801a6f915c5031177d6445f5fb2d0c257ca2d
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-06-04 14:09:14 +0300

    Add Cart Count in CustomAppBarCart

--- lib/features/explor/presention/view/widget/custom_app_bar_cart.dart
+++ lib/features/explor/presention/view/widget/custom_app_bar_cart.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_sphere/core/loading/custom_icon_loading.dart';
 import 'package:shop_sphere/core/utils/app_color.dart';
 import 'package:shop_sphere/core/widget/custom_circle_button.dart';
+import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_cubit.dart';
+import 'package:shop_sphere/features/explor/presention/controller/cart_cubit/cart_state.dart';
 import 'package:shop_sphere/features/profile/presention/view/screen/cart_screen.dart';
 
 class CustomAppBarCart extends StatelessWidget {
@@ -8,35 +12,41 @@ class CustomAppBarCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.topCenter,
-      children: [
-        CustomCircleButton(
-            icon: const Icon(
-              Icons.shopping_cart,
-            ),
-            funcation: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const CartScreen(),
-                  ));
-            }),
-        const Positioned(
-          top: 0,
-          right: 0,
-          child: CircleAvatar(
-              backgroundColor: AppColors.primaryColor,
-              radius: 8,
-              child: Text(
-                '0', // This should be replaced with the actual cart item count
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                ),
-              )),
-        ),
-      ],
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, state) {
+        return state is CartUpdated
+            ? Stack(
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  CustomCircleButton(
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                      ),
+                      funcation: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CartScreen(),
+                            ));
+                      }),
+                   Positioned(
+                    top: 0,
+                    right: 0,
+                    child: CircleAvatar(
+                        backgroundColor: AppColors.primaryColor,
+                        radius: 8,
+                        child: Text(
+                         state.cartProduct.length.toString() , // This should be replaced with the actual cart item count
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        )),
+                  ),
+                ],
+              )
+            : const CustomIconLoading();
+      },
     );
   }
 }
