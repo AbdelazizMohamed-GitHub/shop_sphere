44d877924a23dbe6a78433710c75e225d19757a1
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-06-25 01:02:07 +0300

    Implement getDayOrdersTotalPrice and improve error handling in AnalyticsRepoImpl

--- lib/features/dashboard/data/repo_impl/analytics_repo_impl.dart
+++ lib/features/dashboard/data/repo_impl/analytics_repo_impl.dart
@@ -1,17 +1,35 @@
-  import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shop_sphere/core/errors/fairebase_failure.dart';
+import 'package:shop_sphere/core/service/firestore_service.dart';
 import 'package:shop_sphere/features/dashboard/domain/repo/analytics_repo.dart';
 
 class AnalyticsRepoImpl extends AnalyticsRepo {
+  FirestoreService firestoreService;
+  AnalyticsRepoImpl({required this.firestoreService});
   @override
-  Future<Either<FirebaseFailure, List<int>>> getDayOrdersTotalPrice() {
-    // TODO: implement getDayOrdersTotalPrice
-    throw UnimplementedError();
+  Future<Either<FirebaseFailure, List<int>>> getDayOrdersTotalPrice() async{
+try {
+ List<int> days= await firestoreService.getDayTotal();
+
+  return Right(days);
+} on FirebaseException catch (e) {
+  return Left(FirebaseFailure.fromCode(e.code));
+}on Exception catch (e) {
+  return Left(FirebaseFailure(message: e.toString()));
+}
   }
 
   @override
-  Future<Either<FirebaseFailure, int>> getOrdersTotalPriceTimeRange({required int timeRangeIndex}) {
-    // TODO: implement getOrdersTotalPriceTimeRange
-    throw UnimplementedError();
+  Future<Either<FirebaseFailure, int>> getOrdersTotalPriceTimeRange(
+      {required int timeRangeIndex})async {
+    try {
+      int data=await firestoreService.getOrdersTotalPriceTimeRange(timeRangeIndex: timeRangeIndex);
+      return Right(data);
+    } on FirebaseException catch (e) {
+      return Left(FirebaseFailure.fromCode(e.code));
+    }on Exception catch (e) {
+      return Left(FirebaseFailure(message: e.toString()));
+    }
   }
 }
