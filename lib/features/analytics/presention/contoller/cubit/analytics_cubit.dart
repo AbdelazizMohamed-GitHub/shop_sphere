e5096b58e2a0371665bd735f75b68a2074b45eea
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-07-13 15:38:39 +0300

    Add AnalyticsCubit and AnalyticsState for managing analytics data

---
+++ lib/features/analytics/presention/contoller/cubit/analytics_cubit.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:shop_sphere/features/analytics/data/model/order_over_model.dart';
+import 'package:shop_sphere/features/analytics/data/model/product_most_seller_model.dart';
+import 'package:shop_sphere/features/analytics/domain/repo/analytics_repo.dart';
+
+part 'analytics_state.dart';
+
+class AnalyticsCubit extends Cubit<AnalyticsState> {
+  AnalyticsCubit({required this.analyticsRepo}) : super(AnalyticsInitial());
+  final AnalyticsRepo analyticsRepo;
+
+ 
+  Future<void> getAnalyticsData(int timeRangeIndex, {int limit = 5}) async {
+    emit(AnalyticsLoading());
+
+    final productsResult = await analyticsRepo.getMostSoldProducts(
+      timeRangeIndex: timeRangeIndex,
+      limit: limit,
+    );
+
+    final ordersResult = await analyticsRepo.getOrdersOverTimeRange(
+      timeRangeIndex: timeRangeIndex,
+    );
+
+    // ✅ تحقق من الفشل
+    if (productsResult.isLeft() || ordersResult.isLeft()) {
+      final message = productsResult.fold(
+        (l) => l.message,
+        (r) => null,
+      ) ??
+          ordersResult.fold(
+            (l) => l.message,
+            (r) => null,
+          );
+
+      emit(AnalyticsError(message: message ?? 'Unexpected error'));
+      return;
+    }
+
+    // ✅ البيانات موجودة
+    final mostSoldProducts = productsResult.getOrElse(() => []);
+    final ordersOver = ordersResult.getOrElse(() => []);
+
+    emit(AnalyticsLoaded(
+      mostSoldProducts: mostSoldProducts,
+      ordersOver: ordersOver,
+    ));
+  }
+}
---
+++ lib/features/analytics/presention/contoller/cubit/analytics_state.dart
@@ -0,0 +1,38 @@
+part of 'analytics_cubit.dart';
+
+sealed class AnalyticsState extends Equatable {
+  final List<ProductMostSellerModel> mostSoldProducts;
+  final List<OrderOverModel> ordersOver;
+
+  const AnalyticsState(
+      {required this.mostSoldProducts, required this.ordersOver});
+
+  @override
+  List<Object> get props => [
+        mostSoldProducts,
+        ordersOver,
+      ];
+}
+
+final class AnalyticsInitial extends AnalyticsState {
+  AnalyticsInitial() : super(mostSoldProducts: [], ordersOver: []);
+}
+final class AnalyticsLoading extends AnalyticsState {
+   AnalyticsLoading():super(
+    mostSoldProducts: [],
+    ordersOver: [],
+  );
+}
+final class AnalyticsLoaded extends AnalyticsState {
+  const AnalyticsLoaded({
+    required super.mostSoldProducts,
+    required super.ordersOver,
+  });
+}
+final class AnalyticsError extends AnalyticsState {
+  final String message;
+   AnalyticsError({required this.message}):super(
+    mostSoldProducts: [],
+    ordersOver: [],
+  );
+}
