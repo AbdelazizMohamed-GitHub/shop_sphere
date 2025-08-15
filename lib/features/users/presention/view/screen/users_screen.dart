f227dc8a9af1e253dbb9c0bfe170724a634d474f
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-08-15 16:36:52 +0300

    Refactor UsersScreen layout adjust padding and enhance user interface with consistent grid and list views.

--- lib/features/users/presention/view/screen/users_screen.dart
+++ lib/features/users/presention/view/screen/users_screen.dart
@@ -22,153 +22,185 @@ class _UsersScreenState extends State<UsersScreen> {
   @override
   Widget build(BuildContext context) {
     double horizontalPadding =
-        ResponsiveLayout.getHorizontalLargePadding(context) + 40;
-        final isMobile=ResponsiveLayout.isMobile(context);
+        ResponsiveLayout.getHorizontalLargePadding(context)-40;
+    final isMobile = ResponsiveLayout.isMobile(context);
     return BlocProvider(
       create: (context) =>
           MangeUsersCubit(mangeUsersRepo: getIt<UsersRepoImpl>())
             ..getUsers(isStaff: true),
       child: Builder(
         builder: (context) {
-          return DefaultTabController(
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text("Users"),
-                bottom: TabBar(
-                  onTap: (value) async {
-                    setState(() {
-                      currentIndex = value;
-                    });
-                    if (value == 0) {
-                      await context
-                          .read<MangeUsersCubit>()
-                          .getUsers(isStaff: true);
-                    } else if (value == 1) {
-                      await context
-                          .read<MangeUsersCubit>()
-                          .getUsers(isStaff: false);
-                    }
-                  },
-                  tabs: const [
-                    Tab(text: "Staff"),
-                    Tab(text: "Customers"),
-                  ],
-                ),
-              ),
-              body: BlocBuilder<MangeUsersCubit, MangeUsersState>(
-                  builder: (context, state) {
-                if (state is MangeUsersLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state is MangeUsersFailure) {
-                  return CustomErrorWidget(
-                    errorMessage: state.errMessage,
-                    onpressed: () async {
-                      currentIndex == 0
-                          ? await context
-                              .read<MangeUsersCubit>()
-                              .getUsers(isStaff: true)
-                          : await context
-                              .read<MangeUsersCubit>()
-                              .getUsers(isStaff: false);
+          return WillPopScope(
+            onWillPop: () async {
+              context.goNamed(AppRoute.dashboard);
+              return false; // يمنع الرجوع العادي
+            },
+            child: DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text("Users"),
+                  bottom: TabBar(
+                    onTap: (value) async {
+                      setState(() {
+                        currentIndex = value;
+                      });
+                      if (value == 0) {
+                        await context
+                            .read<MangeUsersCubit>()
+                            .getUsers(isStaff: true);
+                      } else if (value == 1) {
+                        await context
+                            .read<MangeUsersCubit>()
+                            .getUsers(isStaff: false);
+                      }
                     },
-                  );
-                } else if (state is MangeUsersSuccess) {
-                  return state.users.isEmpty
-                      ? const Center(child: Text("No Users Found"))
-                      :isMobile?
-                       ListView.builder(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: horizontalPadding),
-                          itemCount: state.users.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return InkWell(
-                              onTap: () {
-                                currentIndex == 0
-                                    ? context.goNamed(
-                                        AppRoute.staffProductScreen,
-                                        extra: state.users[index].uid)
-                                    : context.goNamed(AppRoute.customerOrders,
-                                        extra: {
-                                            'userId': state.users[index].uid,
-                                            'userName': state.users[index].name,
-                                          });
-                              },
-                              child: Card(
-                                color: Colors.white,
-                                child: ListTile(
-                                    contentPadding: const EdgeInsets.all(16),
-                                    title: Text(state.users[index].name,
-                                        style: AppStyles.text16Bold),
-                                    subtitle: Text(state.users[index].email),
-                                    trailing: IconButton(
-                                      onPressed: () {
-                                        context.goNamed(
-                                            AppRoute.addNotification,
-                                            extra: {
-                                              'fcm':
-                                                  state.users[index].fcmToken,
-                                              'userName':
-                                                  state.users[index].name,
-                                            });
-                                      },
-                                      icon: const Icon(Icons.message_rounded),
+                    tabs: const [
+                      Tab(text: "Staff"),
+                      Tab(text: "Customers"),
+                    ],
+                  ),
+                ),
+                body: BlocBuilder<MangeUsersCubit, MangeUsersState>(
+                    builder: (context, state) {
+                  if (state is MangeUsersLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (state is MangeUsersFailure) {
+                    return CustomErrorWidget(
+                      errorMessage: state.errMessage,
+                      onpressed: () async {
+                        currentIndex == 0
+                            ? await context
+                                .read<MangeUsersCubit>()
+                                .getUsers(isStaff: true)
+                            : await context
+                                .read<MangeUsersCubit>()
+                                .getUsers(isStaff: false);
+                      },
+                    );
+                  } else if (state is MangeUsersSuccess) {
+                    return state.users.isEmpty
+                        ? const Center(child: Text("No Users Found"))
+                        : isMobile
+                            ? ListView.builder(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: horizontalPadding),
+                                itemCount: state.users.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return InkWell(
+                                    onTap: () {
+                                      currentIndex == 0
+                                          ? context.goNamed(
+                                              AppRoute.staffProductScreen,
+                                              extra: state.users[index].uid)
+                                          : context.goNamed(
+                                              AppRoute.customerOrders,
+                                              extra: {
+                                                  'userId':
+                                                      state.users[index].uid,
+                                                  'userName':
+                                                      state.users[index].name,
+                                                });
+                                    },
+                                    child: Card(
+                                      color: Colors.white,
+                                      child: ListTile(
+                                          contentPadding:
+                                              const EdgeInsets.all(16),
+                                          title: Text(state.users[index].name,
+                                              style: AppStyles.text16Bold),
+                                          subtitle:
+                                              Text(state.users[index].email),
+                                          trailing: IconButton(
+                                            onPressed: () {
+                                              context.goNamed(
+                                                  AppRoute.addNotification,
+                                                  extra: {
+                                                    'fcm': state
+                                                        .users[index].fcmToken,
+                                                    'userName':
+                                                        state.users[index].name,
+                                                  });
+                                            },
+                                            icon: const Icon(
+                                                Icons.message_rounded),
+                                          ),
+                                          leading:
+                                              Image.asset(AppImages.profile)),
                                     ),
-                                    leading: Image.asset(AppImages.profile)),
-                              ),
-                            );
-                          },
-                        ):
-                      GridView.builder(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: horizontalPadding),
-                          itemCount: state.users.length,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2),
-                          itemBuilder: (BuildContext context, int index) {
-                            return InkWell(
-                              onTap: () {
-                                currentIndex == 0
-                                    ? context.goNamed(
-                                        AppRoute.staffProductScreen,
-                                        extra: state.users[index].uid)
-                                    : context.goNamed(AppRoute.customerOrders,
-                                        extra: {
-                                            'userId': state.users[index].uid,
-                                            'userName': state.users[index].name,
-                                          });
-                              },
-                              child: Card(
-                                color: Colors.white,
-                                child: ListTile(
-                                    contentPadding: const EdgeInsets.all(16),
-                                    title: Text(state.users[index].name,
-                                        style: AppStyles.text16Bold),
-                                    subtitle: Text(state.users[index].email),
-                                    trailing: IconButton(
-                                      onPressed: () {
-                                        context.goNamed(
-                                            AppRoute.addNotification,
-                                            extra: {
-                                              'fcm':
-                                                  state.users[index].fcmToken,
-                                              'userName':
-                                                  state.users[index].name,
-                                            });
-                                      },
-                                      icon: const Icon(Icons.message_rounded),
+                                  );
+                                },
+                              )
+                            : GridView.builder(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: horizontalPadding),
+                                itemCount: state.users.length,
+                                gridDelegate:
+                                    const SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: 450,mainAxisExtent: 100, // أقصى عرض للكارت
+                                  mainAxisSpacing: 12,
+                                  crossAxisSpacing: 12,
+                                  childAspectRatio: 3,
+                                ),
+                                itemBuilder: (BuildContext context, int index) {
+                                  return InkWell(
+                                    onTap: () {
+                                      currentIndex == 0
+                                          ? context.goNamed(
+                                              AppRoute.staffProductScreen,
+                                              extra: state.users[index].uid)
+                                          : context.goNamed(
+                                              AppRoute.customerOrders,
+                                              extra: {
+                                                'userId':
+                                                    state.users[index].uid,
+                                                'userName':
+                                                    state.users[index].name,
+                                              },
+                                            );
+                                    },
+                                    child: SizedBox(
+                                      width: 300,
+                                      height: 100,
+                                      child: Expanded(
+                                        child: Card(
+                                          color: Colors.white,
+                                          child: ListTile(
+                                              contentPadding:
+                                                  const EdgeInsets.all(12),
+                                              title: Text(state.users[index].name,
+                                                  maxLines: 1,
+                                                  style: AppStyles.text16Bold),
+                                              subtitle:
+                                                  Text(state.users[index].email),
+                                              trailing: IconButton(
+                                                onPressed: () {
+                                                  context.goNamed(
+                                                      AppRoute.addNotification,
+                                                      extra: {
+                                                        'fcm': state.users[index]
+                                                            .fcmToken,
+                                                        'userName': state
+                                                            .users[index].name,
+                                                      });
+                                                },
+                                                icon: const Icon(
+                                                    Icons.message_rounded),
+                                              ),
+                                              leading:
+                                                  Image.asset(AppImages.profile)),
+                                        ),
+                                      ),
                                     ),
-                                    leading: Image.asset(AppImages.profile)),
-                              ),
-                            );
-                          },
-                        );
-                }
-                return Container();
-              }),
+                                  );
+                                },
+                              );
+                  }
+                  return Container();
+                }),
+              ),
             ),
           );
         },
